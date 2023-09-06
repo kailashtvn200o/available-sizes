88f126d2868e6e89b59efebf88939b4eb7d14196
kailashtvn200o <kailashtvn2000@gmail.com>
2023-09-06 16:22:12 +0530

    aaded size chart

--- src/Size.js
+++ src/Size.js
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import './size.css';
 import Footer from './Footer';
-
+import img from './img/image 92.jpg';
 import Header from './Header';
 
 export default function Size() {
-   
+    const [selectedButton, setSelectedButton] = useState(null);
+
+    // Define a function to handle button clicks
+    const handleButtonClick = (button) => {
+        setSelectedButton(button);
+    };
 
     return (
         <>
@@ -15,26 +20,39 @@ export default function Size() {
             <div className='buttonContainer'>
 
                 <div className='rectangle'>
-                    
-                        <div className='rectangle1'>
-                            <button className='rectangletxt1'>
+
+                    <div className='rectangle1'>
+                        <div className='rectangletxt1'>
+                            <div className={selectedButton === 'rectangletxt1' ? 'black' : ''}
+                                onClick={() => handleButtonClick('rectangletxt1')}>
                                 S
-                            </button>
-
-
-                            <button className='rectangletxt1'>
-                                M
-                            </button>
-
-
-                            <button className='rectangletxt1'>
-                                L
-                            </button>
+                            </div>
                         </div>
-                    
+
+
+                        <div className='rectangletxt2'>
+                            <div className={selectedButton === 'rectangletxt2' ? 'black' : ''}
+                                onClick={() => handleButtonClick('rectangletxt2')}>
+                                M
+                            </div>
+                        </div>
+
+
+                        <div className='rectangletxt3'>
+                            <div className={selectedButton === 'rectangletxt3' ? 'black' : ''}
+                                onClick={() => handleButtonClick('rectangletxt3')}>
+                                L
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
+                <div className='sizetxt'>
+                    Size Chart
+                </div>
+                <img className='chart' src={img} />
             </div>
-            <Footer/>
+            <Footer />
         </>
 
 
